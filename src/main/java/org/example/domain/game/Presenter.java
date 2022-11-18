1102b71bba2a0b2077700e46bfee4d14635d4390
devyourown <hyojoon56@gmail.com>
2022-11-19 01:36:42 +0900

    refactor : split functions of Presenter

--- src/main/java/org/example/domain/game/Presenter.java
+++ src/main/java/org/example/domain/game/Presenter.java
@@ -16,28 +16,11 @@ public class Presenter {
     public void giveMoneyToAllInWinner(List<Player> winner, List<Player> loser) {
         winner.sort((a, b) -> pot.getPlayerBetLog(a) - pot.getPlayerBetLog(b));
         for (int i=0; i<winner.size();) {
-            giveWinnerMoney(winner, makeAllInPrizeMoney(loser,
-                    pot.getPlayerBetLog(winner.get(i))));
+            giveWinnerMoney(winner,
+                    makeAllInPrizeMoney(loser, pot.getPlayerBetLog(winner.get(i))));
             pot.takeOutMoney(pot.getPlayerBetLog(winner.get(i)));
             winner.remove(i);
         }
-    }
-
-    private int makeAllInPrizeMoney(List<Player> loser, int winnerBetSize) {
-        int result = 0;
-        for (Player player : loser) {
-            int playerBet = pot.getPlayerBetLog(player);
-            if (playerBet > winnerBetSize) {
-                pot.putPlayerBetLog(player, playerBet - winnerBetSize);
-                pot.takeOutMoney(winnerBetSize);
-                result += winnerBetSize;
-            } else {
-                result += playerBet;
-                pot.takeOutMoney(playerBet);
-                pot.putPlayerBetLog(player, 0);
-            }
-        }
-        return result;
     }
 
     private void giveWinnerMoney(List<Player> winner, int prizeMoney) {
@@ -47,11 +30,32 @@ public class Presenter {
         }
     }
 
-    public void giveMoney(List<Player> winners) {
-        if (isTiedGame(winners)) {
-            for (Player player : winners)
-                player.raiseMoney(getWinnerPrize(winners));
+    private int makeAllInPrizeMoney(List<Player> loser, int winnerBetSize) {
+        int result = 0;
+        for (Player player : loser) {
+            result += getMoneyFromPot(player, winnerBetSize);
         }
+        return result;
+    }
+
+    private int getMoneyFromPot(Player player, int winnerBetSize) {
+        int playerBet = pot.getPlayerBetLog(player);
+        if (playerBet > winnerBetSize) {
+            getMoneyAndLogLeft(player, winnerBetSize, playerBet - winnerBetSize);
+            return winnerBetSize;
+        }
+        getMoneyAndLogLeft(player, playerBet, 0);
+        return playerBet;
+    }
+
+    private void getMoneyAndLogLeft(Player player, int money, int left) {
+        pot.takeOutMoney(money);
+        pot.putPlayerBetLog(player, left);
+    }
+
+    public void giveMoney(List<Player> winners) {
+        if (isTiedGame(winners))
+            giveWinnerMoney(winners, pot.getTotalAmount());
         else
             winners.get(0).raiseMoney(pot.getTotalAmount());
     }
@@ -60,10 +64,5 @@ public class Presenter {
         if (winners.size() > 1)
             return true;
         return false;
-    }
-
-    private int getWinnerPrize(List<Player> winners) {
-        return pot.getTotalAmount() / winners.size() +
-                pot.getTotalAmount() % winners.size();
     }
 }
