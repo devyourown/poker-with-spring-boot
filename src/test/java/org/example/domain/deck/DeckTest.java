cc7ebf7bd357b894d082306632b1d26232f64ad7
devyourown <hyojoon56@gmail.com>
2022-12-12 16:03:41 +0900

    test : add test in DeckTest

--- src/test/java/org/example/domain/deck/DeckTest.java
+++ src/test/java/org/example/domain/deck/DeckTest.java
@@ -12,5 +12,9 @@ public class DeckTest {
         Deck deck = new Deck(3);
         assertEquals(11, deck.getNumOfCards());
         assertEquals(Card.class, deck.draw().getClass());
+        deck = new Deck(8);
+        assertEquals(21, deck.getNumOfCards());
+        deck = new Deck(4);
+        assertEquals(13, deck.getNumOfCards());
     }
 }
